7698fa9b984c7b0792ab5aa8a82e7d257cab8ecf
czhan125 <czhan125@ubc.ca>
2024-10-06 22:21:20 -0700

    setter methods implement

--- src/main/model/DrinkRecord.java
+++ src/main/model/DrinkRecord.java
@@ -21,4 +21,15 @@ public class DrinkRecord {
     public void setAmount(int newAmount) {
         amount = newAmount;
     }
+
+    // EFFECTS:  returns the DrinkRecord's type
+    public String getType() {
+        return type;
+    }
+
+    // EFFECTS:  returns the DrinkRecord's amount
+    public int getAmount() {
+        return amount;
+    }
+
 }
