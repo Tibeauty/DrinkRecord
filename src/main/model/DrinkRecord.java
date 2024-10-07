e0dd2937035e300562553e051092fc0b09be9a55
czhan125 <czhan125@ubc.ca>
2024-10-06 22:16:11 -0700

    implement setter function in drinkrecord class

--- src/main/model/DrinkRecord.java
+++ src/main/model/DrinkRecord.java
@@ -4,9 +4,21 @@ public class DrinkRecord {
     private String type;       //Types of beverages
     private int amount;        //Drink amount of this drinkrecord
 
-
+    // EFFECTS: constructs a DrinkRecord object 
     public DrinkRecord(String type, int amount) {
         this.type = type;
         this.amount = amount;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the DrinkRecord type to the new, given type
+    public void setType(String newtype) {
+        type = newtype;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the DrinkRecord amount to the new, given amount
+    public void setAmount(int newAmount) {
+        amount = newAmount;
     }
 }
