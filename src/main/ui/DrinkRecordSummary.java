cd2f8bced31bb0edf21c71da730acb8654013e03
czhan125 <czhan125@ubc.ca>
2024-10-06 22:29:37 -0700

    add new drinkrecord summary class

---
+++ src/main/ui/DrinkRecordSummary.java
@@ -0,0 +1,17 @@
+package ui;
+
+import model.DrinkRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DrinkRecordSummary {
+    private List<DrinkRecord> drinkRecords;
+
+    public DrinkRecordSummary() { 
+        this.drinkRecords = new ArrayList<>();
+
+    }
+
+}
