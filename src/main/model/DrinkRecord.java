4a91a3770b2f9af69c205e9e379ab0cdd819ad6a
czhan125 <czhan125@ubc.ca>
2024-10-06 22:03:26 -0700

    drinkrecord class create and constructor implement

---
+++ src/main/model/DrinkRecord.java
@@ -0,0 +1,12 @@
+package model;
+
+public class DrinkRecord {
+    private String type;       //Types of beverages
+    private int amount;        //Drink amount of this drinkrecord
+
+
+    public DrinkRecord(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+}
