0a4fbd3e9c374ba53c8d97643ffc8caa2f83288d
czhan125 <czhan125@ubc.ca>
2024-10-10 19:22:59 -0700

    add getter and setter functions for DrinkRecords

--- src/main/model/DrinkRecords.java
+++ src/main/model/DrinkRecords.java
@@ -17,4 +17,19 @@ public class DrinkRecords {
     public void addDrinkRecord(DrinkRecord drinkRecord) {
         this.drinkRecords.add(drinkRecord);
     }
+
+    // EFFECTS: returns true if there are no drink records, false otherwise
+    public boolean isEmpty() {
+        return this.drinkRecords.isEmpty();
+    }
+
+    // EFFECTS: returns the size of the drink records list
+    public int size() {
+        return this.drinkRecords.size();
+    }
+
+    // EFFECTS: returns the drink record at a specified index
+    public DrinkRecord getRecordAtIndex(int index) {
+        return this.drinkRecords.get(index);
+    }
 }
