5df865680ad1a312a2740488458933591ebe5a1f
czhan125 <czhan125@ubc.ca>
2024-10-21 02:16:50 -0700

    implement all methods in JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,6 +1,8 @@
 package persistence;
 
 import model.DrinkRecords;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
@@ -14,30 +16,32 @@ public class JsonWriter {
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of DrinkRecords to file
     public void write(DrinkRecords drinkRecords) {
-
+        JSONArray json = drinkRecords.drinkRecordsToJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
+        writer.print(json);
     }
 }
