082e76f09e27eadfa957c16a2032f71e5c46e0aa
czhan125 <czhan125@ubc.ca>
2024-10-21 02:26:39 -0700

    implement methods in JsonReader

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 // Represents a reader that reads DrinkRecords from JSON data and stored in file
@@ -17,29 +18,45 @@ public class JsonReader {
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-
+        this.source = source;
     }
 
     // EFFECTS: reads DrinkRecords from file and returns it;
     // throws IOException if an error occurs reading data from file
     public DrinkRecords read() throws IOException {
-
+        String jsonData = readFile(source);
+        JSONArray jsonArray = new JSONArray(jsonData);
+        DrinkRecords drinkRecords = new DrinkRecords();
+        return addDrinkRecords(drinkRecords, jsonArray);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
+        StringBuilder contentBuilder = new StringBuilder();
 
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // MODIFIES: drinkRecords
     // EFFECTS: parses drink records from JSON object and adds them to the list
-    private void addDrinkRecords(DrinkRecords drinkRecords, JSONObject jsonObject) {
-
+    private DrinkRecords addDrinkRecords(DrinkRecords drinkRecords, JSONArray jsonArray) {
+        for (Object json : jsonArray) {
+            JSONObject nextThingy = (JSONObject) json;
+            addDrinkRecord(drinkRecords, nextThingy);
+        }
+        return drinkRecords;
     }
 
      // MODIFIES: drinkRecords
     // EFFECTS: parses a single drink record from JSON object and adds it to the list
     private void addDrinkRecord(DrinkRecords drinkRecords, JSONObject jsonObject) {
-
+        String type = jsonObject.getString("type");
+        int amount = jsonObject.getInt("amount");
+        DrinkRecord drinkRecord = new DrinkRecord(type, amount);
+        drinkRecords.addDrinkRecord(drinkRecord);
     }
 }
