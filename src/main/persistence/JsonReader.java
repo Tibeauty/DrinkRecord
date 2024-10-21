9c62e70548f4dc692962d2a8e2e96f935a366589
czhan125 <czhan125@ubc.ca>
2024-10-21 01:21:35 -0700

    specify methods in jsonreader

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,5 +1,45 @@
 package persistence;
 
-public class JsonReader {
+import model.*;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.json.JSONObject;
+
+// Represents a reader that reads DrinkRecords from JSON data and stored in file
+public class JsonReader {
+    private String source;
+
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader(String source) {
+
+    }
+
+    // EFFECTS: reads DrinkRecords from file and returns it;
+    // throws IOException if an error occurs reading data from file
+    public DrinkRecords read() throws IOException {
+
+    }
+
+    // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+
+    }
+
+    // MODIFIES: drinkRecords
+    // EFFECTS: parses drink records from JSON object and adds them to the list
+    private void addDrinkRecords(DrinkRecords drinkRecords, JSONObject jsonObject) {
+
+    }
+
+     // MODIFIES: drinkRecords
+    // EFFECTS: parses a single drink record from JSON object and adds it to the list
+    private void addDrinkRecord(DrinkRecords drinkRecords, JSONObject jsonObject) {
+
+    }
 }
