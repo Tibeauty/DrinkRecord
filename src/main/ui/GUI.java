e69cb5d4c81b378bd32224664109fc1494d0fa6a
czhan125 <czhan125@ubc.ca>
2024-11-15 03:54:05 -0800

    add label and button

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,28 +1,48 @@
 package ui;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //the graphical user interface for drinking records
-public class GUI {
+public class GUI implements ActionListener {
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JButton button;
+    private JPanel panel;
+
     public GUI() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
 
-        
-
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(600, 600, 300, 600));
+        button = new JButton("add");
+        label = new JLabel("Number of drinking records: 0");
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        button.addActionListener(this);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Drining Records");
         frame.pack();
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of drinking records:" + count);
     }
 }
